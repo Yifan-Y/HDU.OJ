f84947a38430c9326035152fffecec7051a03d7f
Yifan Yang <36191021+Yifan-Y@users.noreply.github.com>
2018-05-03 20:29:19 +0800

    Update 1003.c

--- 1003.c
+++ 1003.c
@@ -1,3 +1,8 @@
+//Given a sequence a[1],a[2],a[3]......a[n].
+//Your job is to calculate the max sum of a sub-sequence. 
+//For example, given (6,-1,5,4,-7), the max sum in this sequence is 6 + (-1) + 5 + 4 = 14.
+//Yifan Yang
+
 #include <stdio.h>
 #include <stdlib.h>
 
